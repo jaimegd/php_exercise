b1973c4cc14dd26ed80d724c65a8000324c75a83
JaimeG <jaimeg@ubuntu.(none)>
2013-12-06 12:00:53 -0500

    v1

---
+++ README.md
@@ -0,0 +1,78 @@
+Hi,
+
+Instead of having the code on Worp I decided to put all the code in the 
+file index.php.
+
+So you can run on the terminal: php index.php # [params]
+Example:
+
+For question 1: 
+php index.php 1
+
+
+For question 2: 
+php index.php 2 5
+
+
+For question 3: 
+php index.php 3 4
+
+
+For question 4: 
+php index.php 4 10 2,4,5,8,10,20,30
+
+In this case you are executing function 4, sending 10 as the number to look
+and you can send an array in this case: 2,4,5,8,10,20,30
+
+
+For question 5, 6 and 7: 
+php index.php 5
+php index.php 6
+php index.php 7
+
+
+For question 8: 
+php index.php 8
+
+
+For question 9: 
+php index.php 9 16
+
+
+For question 10: 
+php index.php 10
+
+
+For question 11: 
+php index.php 11 good
+php index.php 11 'even better'
+
+You can send an string with more than 1 word using ''
+
+
+For question 12: 
+php index.php 12
+
+
+For question 13: 
+php index.php 13
+
+Please open the file lightbox.html
+
+
+For question 14: 
+php index.php 14
+
+You can paste this code in a JS terminal:
+
+str_1 = "------- The quick brown fox jumps over the lazy dog -------";			
+
+function custom_str_replace(str){ 
+return str.replace("The quick brown fox jumps over the lazy dog","The1 quick2 brown3 fox4 jumps5 over6 the7 lazy8 dog9");
+}
+
+str_2 = custom_str_replace(str_1);
+
+
+For question 15: 
+php index.php 15
--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 $function = isset($argv[1]) ? $argv[1]: '';
 
-switch($function)
-{
+print PHP_EOL.PHP_EOL;
+
+switch($function){
 	case 1:
 		print 'Passwords must be saved Encrypted'.PHP_EOL.PHP_EOL;;
 		print 'PHP has different methods like md5() or sha1(), normally I use md5';
@@ -11,11 +12,8 @@ switch($function)
 	case 2:
 		$number = isset($argv[2]) ? $argv[2]: '';
 		if(empty($number))
-		{
 			print 'You need to send a 2nd parameter to get the nth Fibonacci item';	
-		}
-		else
-		{
+		else{
 			$fibonacci = fibonacci($number);
 			print 'Fibonacci of '.$number.' is: '.$fibonacci;
 		}
@@ -23,23 +21,17 @@ switch($function)
 	case 3:
 		$number = isset($argv[2]) ? $argv[2]: '';
 		if(empty($number))
-		{
-			print 'You need to send a 2nd parameter to get the nth prime Fibonacci item';	
-		}
-		else
-		{
+			print 'You need to send a 2nd parameter to get the nth prime Fibonacci item';			
+		else{
 			$fibonacci = fibonacci_extended_prime($number);
 			print 'Prime # '.$number.' of the fibonacci serie: '.$fibonacci;
 		}
-
 	break;
 	case 4:
 		$probe = '';
 		$number = isset($argv[2]) ? $argv[2]: '';
 		if(empty($number))
-		{
 			print 'You need to send a 2nd parameter to get the position of that number';	
-		}
 		else
 		{
 			$list = isset($argv[3]) ? explode(',', $argv[3]) : array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
@@ -60,11 +52,57 @@ switch($function)
 		print 'a) Injection vulnerabilities and cross-site scripting'.PHP_EOL.PHP_EOL;
 		print 'b) Broken authentication and session management'.PHP_EOL.PHP_EOL;
 		print 'c) Insecure direct object references'.PHP_EOL.PHP_EOL;
-
 		print 'Source: http://searchsecurity.techtarget.com/tip/Five-common-Web-application-vulnerabilities-and-how-to-avoid-them'.PHP_EOL;
-		print 'Source: http://stackoverflow.com/questions/1267284/common-website-attack-methods-detection-and-recovery'.PHP_EOL.PHP_EOL;
+		print 'Source: http://stackoverflow.com/questions/1267284/common-website-attack-methods-detection-and-recovery'.PHP_EOL.PHP_EOL;	
+	break;
+	case 9:
+		$number = isset($argv[2]) ? $argv[2]: '';
+		if(is_power_of_2($number))
+			print $number.' is power of 2'.PHP_EOL.PHP_EOL;
+		else
+			print $number. ' is not power of 2'.PHP_EOL.PHP_EOL;
+	break;
+	case 10:
+		print 'Applyin Regex either on the Server with PHP'.PHP_EOL;
+		print 'or on the client with Javascript.'.PHP_EOL.PHP_EOL;
+		print 'Example PHP: http://stackoverflow.com/questions/6363108/php-changing-phone-number-format-with-regex'.PHP_EOL;
+		print 'Examplo JS: http://stackoverflow.com/questions/8358084/regular-expression-to-reformat-a-phone-number-in-javascript'.PHP_EOL;
+	break;
+	case 11:
+		$string = isset($argv[2]) ? $argv[2]: '';
+		print 'String reverse: '.strrev($string).PHP_EOL.PHP_EOL;
+	break;
+	case 12:
+		print 'I didn\'t understan very well this point, however I think something like this would work'.PHP_EOL.PHP_EOL;
+		print 'function object(o) { '.PHP_EOL.'
+		        function F() {}'.PHP_EOL.'
+		        F.prototype = o;'.PHP_EOL.'
+		        return new F();
+		    }'.PHP_EOL.PHP_EOL;
+		print 'Source: http://javascript.crockford.com/prototypal.html';
+	break;
+	case 13:
+		print 'Please open the file "lightbox.html" in a browser'.PHP_EOL.PHP_EOL;
+		print 'Source: http://lokeshdhakar.com/projects/lightbox2/';		
+	break;
+	case 14:		
+		print 'Javascript Code: '.PHP_EOL.PHP_EOL;
+		print 'You can run the next code in any JS Console [chrome, firefox, etc]'.PHP_EOL;
+		print '
+			str_1 = "------- The quick brown fox jumps over the lazy dog -------";			
 
-	
+			function custom_str_replace(str){ 
+				return str.replace("The quick brown fox jumps over the lazy dog","The1 quick2 brown3 fox4 jumps5 over6 the7 lazy8 dog9");
+			}
+
+			str_2 = custom_str_replace(str_1);
+		';
+	case 15:
+		print 'To mention a few of them:'.PHP_EOL.PHP_EOL;
+		print '2D drawing'.PHP_EOL;
+		print 'Caching'.PHP_EOL;
+		print 'Video'.PHP_EOL.PHP_EOL;
+		print 'Source: http://www.ibm.com/developerworks/library/wa-html5fundamentals3/';
 	break;
 	default:
 		print '================INVALID FUNCTION ================'.PHP_EOL.PHP_EOL;
@@ -80,17 +118,12 @@ print PHP_EOL.PHP_EOL;
  * @return int
  * Function Source: http://www.codewalkers.com/c/a/Miscellaneous/Recursion-in-PHP/2/
  */
-function fibonacci($number)
-{
+function fibonacci($number){
 	// function to output the nth item in a Fibonacci series.
 	if ($number == 1 || $number == 2)
-	{
 		return 1;
-	}
 	else
-	{
-		return fibonacci( $number - 1) + fibonacci( $number - 2 );
-	}
+		return fibonacci( $number - 1) + fibonacci( $number - 2 );	
 }
 
 
@@ -99,8 +132,7 @@ function fibonacci($number)
  * @param int $number
  * @return int
  */
-function fibonacci_extended_prime($number)
-{
+function fibonacci_extended_prime($number){
 	$got_it = false;
 	$i = 1; 
 	$fib = 0;
@@ -121,7 +153,7 @@ function fibonacci_extended_prime($number)
  * @return boolean
  * Function Source: http://icdif.com/computing/2011/09/15/check-number-prime-number/
  */
-function isPrime($num) {
+function isPrime($num){
     if($num == 1)
         return false;
     if($num == 2)
@@ -141,8 +173,7 @@ function isPrime($num) {
  * @return boolean
  * Function Source: http://us1.php.net/array_search
  */
-function Array_BinarySearch( $needle, $haystack, $comparator , &$probe )
-{
+function Array_BinarySearch( $needle, $haystack, $comparator , &$probe ){
     $high = Count( $haystack ) -1;
     $low = 0;
     while ( $high >= $low )
@@ -161,9 +192,25 @@ function Array_BinarySearch( $needle, $haystack, $comparator , &$probe )
     return false;
 }
 
-function CompareNumbers($obj, $needle)
-{
+/**
+ * Function to execute Binary comparation
+ * @param int $obj, int $needle
+ * @return boolean
+ * Function Source: http://us1.php.net/array_search
+ */
+function CompareNumbers($obj, $needle){
     return $obj - $needle;
 }
 
+/**
+ * Function to determine if a number is power of 2
+ * @param int $number
+ * @return boolean
+ * Function Source: http://stackoverflow.com/questions/4965301/finding-if-a-number-is-a-power-of-2
+ */
+function is_power_of_2($number){
+	if ( $number > 0 & (($number-1) & $number) == 0)
+		return true;
+	return false;
+}
 ?>
